fb696078466ac94e6c5b5dbec654f5cf4ce6649a
Luna Hazuki <LunaHazuki2006@outlook.com>
2024-10-25 16:25:39 -0400

    aaaaaaaaaaa

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ def creacion(r : list):
     rango = maximo - minimo 
     k = 1 + 3.3 * math.log(n, 10)
     amplitud = rango / k
-    if amplitud > int(amplitud): amplitud = int(amplitud)
+    print(f'amplitud: {amplitud}')
+    if amplitud > int(amplitud): 
+        # amplitud += 1
+        # amplitud = int(amplitud)
+        amplitud = round(amplitud)
+    print(f'amplitud: {amplitud}')
     lista = {}
     for esto in r: 
         try: lista[esto] += 1
@@ -39,6 +44,7 @@ def creacion(r : list):
     oficial[0]['fa'] = parte
     oficial[0]['xi'] = list(map(lambda x: (x['minimo'] + x['maximo']) / 2, oficial[0]['clase']))
     oficial[0]['fi.xi'] = [(xi * fi) for xi, fi in zip(oficial[0]['fi'], oficial[0]['xi'])]
+    print(f'suma fixi {sum(oficial[0]["fi.xi"])}')
     oficial[0]['fsr'] = list(map(lambda x: x / oficial[0]['fa'][-1], oficial[0]['fi']))
     oficial[0]['far'] = list(map(lambda x: x / oficial[0]['fa'][-1], oficial[0]['fa']))
     oficial[0]['fsr%'] = list(map(lambda x: x * 100, oficial[0]['fsr']))
